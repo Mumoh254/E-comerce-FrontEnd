579c66e3f226915142092867767bd69216724cf2
peter mumo <135279804+Mumoh254@users.noreply.github.com>
2025-02-22 14:28:11 -0800

    updates

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
           <p className="mb-0" style={{ fontSize: "10px" }}>
             Hotline:{" "}
             <a className="fw-bold  text-text-decoration-none" href="tel:+254740045355" style={{ color: "var(--color-sample-1)", fontSize: "10px" }}>
-               +254740045355  
+               +254740045355  |
             </a>
           </p>
         </div>
